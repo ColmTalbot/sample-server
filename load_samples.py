b0a2a9cad7987e7f4effd91988ba904932570116
Colm Talbot <talbotcolm@gmail.com>
2024-05-31 15:02:14 -0500

    Initial commit

---
+++ load_samples.py
@@ -0,0 +1,46 @@
+import h5py
+import numpy as np
+
+
+async def load_samples(
+    filename: str,
+    model: str,
+    variables: list[str],
+    n_samples: int,
+    seed: int | None = None,
+):
+    output = dict()
+    with h5py.File(filename, "r") as ff:
+        data = ff[model]["posterior_samples"]
+
+        total_n_samples = len(data)
+        if n_samples > total_n_samples:
+            raise ValueError(
+                f"Insufficient samples available for {model} in {filename}. "
+                f"{n_samples} requested and {total_n_samples} available."
+            )
+        elif n_samples == -1:
+            idxs = np.arange(total_n_samples)
+        else:
+            rng = np.random.default_rng(seed)
+            idxs = rng.choice(total_n_samples, n_samples, replace=False)
+
+        output["idxs"] = list(idxs)
+        output["samples"] = dict()
+        for variable in variables:
+            try:
+                output["samples"][variable] = list(data[variable][idxs])
+            except ValueError:
+                raise KeyError(
+                    f"Variable {variable} not found in the posterior samples. "
+                    f"Available variables are: {data.dtype}"
+                )
+    output["model"] = model
+    return output
+
+
+def find_variables(filename, model):
+    with h5py.File(filename, "r") as ff:
+        data = ff[model]["posterior_samples"]
+        return data.dtype.names
+
---
+++ main.py
@@ -0,0 +1,98 @@
+import re
+from pathlib import Path
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Request, Query
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.gzip import GZipMiddleware
+from pydantic import BaseModel
+
+from load_samples import load_samples, find_variables
+
+app = FastAPI()
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+result_dir = Path("/home/sample-user/samples")
+
+
+class SampleDict(BaseModel):
+    model: str
+    idxs: list[int]
+    samples: dict[str, list[float]]
+
+
+EVENTS = [
+    re.findall(r"GW[\d]{6}_[\d]+", str(fname))[0]
+    for fname in result_dir.iterdir() if "GW" in str(fname)
+]
+FILENAMES = {
+    str(re.findall(r"GW[\d]{6}_[\d]+", str(fname))[0]): fname
+    for fname in result_dir.iterdir() if "GW" in str(fname)
+}
+
+
+@app.get("/events")
+async def list_events(request: Request) -> list[str]:
+    return EVENTS
+
+
+@app.get("/events/{event}/")
+async def read_samples(
+    request: Request,
+    event: str,
+    variable: Annotated[list[str] | None, Query()] = None,
+    n_samples: int = -1,
+    model: str = "C01:IMRPhenomXPHM",
+    seed: int | None = None,
+) -> SampleDict | list[str]:
+    filename = FILENAMES.get(event, None)
+
+    if filename is None:
+        raise HTTPException(
+            status_code=404,
+            detail=(
+                f"Sample file {filename} for {event} not found. "
+                f"Available values are {', '.join(EVENTS)}. "
+                f"{FILENAMES}",
+            ),
+        )
+
+    if variable is None:
+        return find_variables(filename, model)
+
+    return await _read_samples(
+        filename=filename,
+        model=model,
+        variables=variable,
+        n_samples=n_samples,
+        seed=seed,
+    )
+
+
+async def _read_samples(
+    filename: Path | None,
+    variables: list[str],
+    n_samples: int = -1,
+    model: str = "C01:IMRPhenomXPHM",
+    seed: int | None = None,
+):
+    if filename is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Unknown file {filename}: {', '.join(FILENAMES.keys())}",
+        )
+    try:
+        data = await load_samples(
+            filename=filename,
+            model=model,
+            variables=variables,
+            n_samples=n_samples,
+            seed=seed,
+        )
+    except (ValueError, KeyError) as e:
+        raise HTTPException(status_code=404, detail=str(e).strip('"'))
+
+    return data
+
+
